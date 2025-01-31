d160e8b368c202308be4d901cca6853844639f68
Your Name <your-email@example.com>
2025-01-31 23:05:16 +0530

    Updates

---
+++ precache-manifest.582a72fe1927ccf8a67a72d2886ee838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0933b5abdeac90cf2d818f9ddd4c6fd3",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "524fe61bd5145962491c",
+    "url": "/chat-app/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "bac9ded9685a43a988db",
+    "url": "/chat-app/static/js/2.c2536c2e.chunk.js"
+  },
+  {
+    "revision": "01f3ca819afe02d7b35b3db1095e3367",
+    "url": "/chat-app/static/js/2.c2536c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bffdf3730c856e33ddd",
+    "url": "/chat-app/static/js/3.79ac2144.chunk.js"
+  },
+  {
+    "revision": "524fe61bd5145962491c",
+    "url": "/chat-app/static/js/main.1ee69b09.chunk.js"
+  },
+  {
+    "revision": "dabec535d6dad1a48687",
+    "url": "/chat-app/static/js/runtime-main.c027c87d.js"
+  }
+]);
